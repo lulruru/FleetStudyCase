d3a683a6988e83b342ff0309af64176d72e4c895
Russel Encarnacion <russel.enc@gmail.com>
2026-03-29 14:55:17 +0200

    feat: add products/cart/orders to SQLite

--- server/index.js
+++ server/index.js
@@ -38,6 +38,45 @@ db.serialize(() => {
       FOREIGN KEY (owner_id) REFERENCES employees(id) ON DELETE SET NULL
     )
   `);
+  db.run(`
+    CREATE TABLE IF NOT EXISTS products (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT NOT NULL,
+	  description TEXT,
+	  price REAL NOT NULL,
+	  stock INTEGER NOT NULL DEFAULT 0,
+	  created_at TEXT DEFAULT CURRENT_TIMESTAMP
+    )
+  `);
+  db.run(`
+	CREATE TABLE IF NOT EXISTS cart_items (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+	  product_id INTEGER NOT NULL,
+      quantity INTEGER NOT NULL DEFAULT 1,
+      created_at TEXT DEFAULT CURRENT_TIMESTAMP,
+      FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE
+    )
+  `);
+  db.run(`
+	CREATE TABLE IF NOT EXISTS orders (
+	  id INTEGER PRIMARY KEY AUTOINCREMENT,
+	  total REAL NOT NULL,
+	  created_at TEXT DEFAULT CURRENT_TIMESTAMP
+	)
+  `);
+  db.run(`
+	CREATE TABLE IF NOT EXISTS order_items(
+	  id INTEGER PRIMARY KEY AUTOINCREMENT,
+	  order_id INTEGER NOT NULL,
+	  product_id INTEGER NOT NULL,
+	  product_name TEXT NOT NULL,
+	  product_price REAL NOT NULL,
+	  quantity INTEGER NOT NULL,
+	  FOREIGN KEY (order_id) REFERENCES orders(id) ON DELETE CASCADE
+	)
+  `);
+	
+
 });
 
 app.get("/api/health", (req, res) => {
